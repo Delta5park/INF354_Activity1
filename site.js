da3f18fa40a851900132e82fa099bd3991f03946
Delta5park <38748214+Delta5park@users.noreply.github.com>
2019-02-07 11:44:56 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,9 @@
 function msg(){  
  alert("I AM EXCITED FOR INF 354 as I shall learn alot and hopefully look better while looking for a job in industry");  
 } 
+function msg1(){  
+ alert("I AM EXCITED FOR INF 354 !!!");  
+} 
+function msg2(){  
+ alert("excited to learn some crazy coding! :)");  
+} 

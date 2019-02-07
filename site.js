f53ef73a3653f36a00f4a1ea4e35a560f905c3f0
ViljoenJ <47412795+ViljoenJ@users.noreply.github.com>
2019-02-07 11:43:20 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,3 @@
 function msg(){  
- alert("I AM EXCITED FOR INF 354 as I shall learn alot and hopefully look better while looking for a job in industry");  
+ alert("excited to learn some crazy coding! :)");  
 } 
